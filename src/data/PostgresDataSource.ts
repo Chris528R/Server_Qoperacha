9f8741da864c8e152071c19db2d8cbdca03f9237
Mendoza Castañeda José Ricardo <joseanakin@outlook.es>
2025-09-22 13:50:32 -0600

    ENV configuration finished

--- src/data/PostgresDataSource.ts
+++ src/data/PostgresDataSource.ts
@@ -1,2 +1,22 @@
 import "reflect-metadata"
 
+import { DataSource } from 'typeorm'
+import { ENVS } from "../plugins/env.plugin.js"
+
+export const PostgresDataSource = new DataSource({
+    
+    type: "postgres",
+
+    host: ENVS.DB_HOST,
+    port: ENVS.DB_PORT,
+    username: ENVS.DB_USER,
+    password: ENVS.DB_PASSWORD,
+    database: ENVS.DB_NAME,
+
+    synchronize: true,
+    logging: true,
+    entities: [],
+    subscribers: [],
+    migrations: []
+
+})
---
+++ src/plugins/env.plugin.ts
@@ -0,0 +1,18 @@
+import 'dotenv/config'
+import * as env from 'env-var'
+
+export const ENVS = {
+    // Server PORT and public data.
+    PORT: env.get('PORT').required().asPortNumber(),
+    PUBLIC_PATH: env.get('PUBLIC_PATH').asString(),
+
+    // Database Connection.
+    DB_USER: env.get('DB_USER').required().asString(),
+    DB_PASSWORD: env.get('DB_PASSWORD').required().asString(),
+    DB_HOST: env.get('DB_HOST').required().asString(),
+    DB_PORT: env.get('DB_PORT').required().asPortNumber(),
+    DB_NAME: env.get('DB_NAME').required().asString(),
+
+    // Secret Key of Jsonwebtoken.
+    SECRET_KEY: env.get('SECRET_KEY').required().asString()
+}
