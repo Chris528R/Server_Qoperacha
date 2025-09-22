5bebce65e3ef4a101564fc19a07f0d0aabcd88cb
Chris528R <crodriguezm2001@alumno.ipn.mx>
2025-09-22 13:25:18 -0600

    Cambios

---
+++ src/presentation/services/PaymentService.ts
@@ -0,0 +1,12 @@
+export interface IPaymentService {
+
+    // Usuario, A que fondo., monto
+    // sDevolver todo lo de la api :)
+    // doPayment();
+
+    //Usuario, a que fondo, monto, cada cuando
+    // Devolver todo lo de la api
+    // doRecurrentPayment();
+
+}
+
